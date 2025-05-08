a34795022c654a2036bf3a6e5b91e24ef7ce0f83
Ch27s <dbcotmd0@gmail.com>
2025-05-08 09:49:30 +0900

    채승 : (add)writeView / (edit)writeController

--- src/SimpleBoard/view/WriteView.java
+++ src/SimpleBoard/view/WriteView.java
@@ -1,5 +1,36 @@
 package SimpleBoard.view;
 
+import java.util.Scanner;
+
+import SimpleBoard.dto.BoardDTO;
+
 public class WriteView {
+
+	public BoardDTO writeMenu() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("작성자>> ");
+		String writer = sc.nextLine();
+		System.out.print("제목>> ");
+		String title = sc.nextLine();
+		System.out.print("내용>> ");
+		String content = sc.nextLine();
+		
+		BoardDTO dto = BoardDTO.builder()
+				.writer(writer)
+				.title(title)
+				.content(content)
+				.build();
+		
+		return dto;
+	}
+	
+	public void Message(int result, String ms) {
+		
+		String insertMessage = result + ms;
+		
+		System.out.println(insertMessage);
+		
+	}
 
 }
